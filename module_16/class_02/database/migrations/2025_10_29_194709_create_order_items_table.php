455fcadd69f4f581e1b7d0cc9b9066c4a83eea10
RobiulHassan0 <robiulhassan270@gmail.com>
2025-10-30 22:43:59 +0600

    explore migration more

--- module_16/class_02/database/migrations/2025_10_29_194709_create_order_items_table.php
+++ module_16/class_02/database/migrations/2025_10_29_194709_create_order_items_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->foreign('order_id')
                 ->references('id')->on('order-books')
                 ->onUpdate('cascade')
-                ->onDelete('');
+                ->onDelete('cascade');
 
             $table->foreign('book_id')
                 ->references('id')->on('books')
