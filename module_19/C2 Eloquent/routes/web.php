98750731523f3bbb9c624c32fa22ddc7502f2fff
RobiulHassan0 <robiulhassan270@gmail.com>
2025-12-13 01:26:06 +0600

    learning eloquent

---
+++ module_19/C2 Eloquent/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\TestController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/oneToOne', [TestController::class, 'oneToOne']);
+Route::get('/oneToMany', [TestController::class, 'oneToMany']);
+Route::get('/manyToMany', [TestController::class, 'manyToMany']);
+Route::get('/productWithCat', [TestController::class, 'productWithCat']);
+Route::get('/manyToManyTest', [TestController::class, 'manyToManyTest']);
+Route::get('/selfReff', [TestController::class, 'selfReff']);
