f3e35bff10436c18ddacb453fb7e312cf83ef5c2
RobiulHassan0 <robiulhassan270@gmail.com>
2026-02-11 16:53:07 +0600

    product controller done

---
+++ module_24/reapeat_inventory/app/Http/Controllers/Auth/V1/StockMovementController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Auth\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StockMovementController extends Controller
+{
+    public function index(){
+        
+    }
+}
