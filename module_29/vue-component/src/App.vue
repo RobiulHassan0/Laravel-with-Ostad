cadfcd2fdcfc3f2a07cc95c29d52b911ad5eab92
RobiulHassan0 <robiulhassan270@gmail.com>
2026-05-17 20:22:34 +0600

    start vue class 4

---
+++ module_29/vue-component/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import GreetingCard from './components/greetingCard.vue';
+
+</script>
+
+<template>
+<h2>This is App vue file</h2>
+  <GreetingCard>
+
+  </GreetingCard>
+
+</template>
+
+<style scoped></style>
---
+++ module_29/vue-component/src/components/GreetingCard.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h2>This is component</h2>
+    </div>
+</template>
+
+<style scoped></style>
